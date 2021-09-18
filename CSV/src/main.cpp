d7017862fa7649450b9edaef71dddbbe4cad066d
Amr Hassan <amrtechguy@gmail.com>
2021-09-18 23:03:12 +0200

    Added the CSV class
    Now I can write to a CSV file

--- CSV/src/main.cpp
+++ CSV/src/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+class CSV
+{
+private:
+    std::string file_path;
+    std::ofstream file;
+
+public:
+    CSV(std::string file_path)
+        : file_path {file_path}
+    {
+        file.open(this->file_path, std::ios::app);
+        
+        if(!file.is_open())
+        {
+            std::cout << "File [" << this->file_path << "]  is not found!" << std::endl;
+        }
+    }
+
+    void write(std::string line)
+    {
+        if(!this->file.is_open())
+        {
+            std::cout << "File [" << this->file_path << "]  is not found!" << std::endl;
+        }
+        else
+        {
+            this->file << line << std::endl;
+        }
+    }
+
+    ~CSV()
+    {
+        this->file.close();
+    }
+};
 
 int main()
 {
-
+    CSV csv_file {"data.csv"};
+    
+    csv_file.write("Amr Hassan,Software Engineer,Egypt");
 
     return 0;
 }
