0053a48780098131ffb76a7124dd5502de4b2455
Amr Hassan <amrtechguy@gmail.com>
2021-09-19 19:47:29 +0200

    Started HTML Generator Project
    Added a basic structure

---
+++ 1.HTML_Generator/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+class HTML
+{
+private:
+    
+
+public:
+
+};
+
+int main()
+{
+    std::cout << "This is HTML generator!" << std::endl;
+    std::cout << "Work still in progress ... " << std::endl;  
+
+    return 0;
+}
